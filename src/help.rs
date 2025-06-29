bc4f19fc49d4c76e4ca2ec876a60f6f529430fdf
Michus3142 <mstrojek8@gmail.com>
2025-06-29 23:35:45 +0200

    Update: Can pass args

---
+++ src/help.rs
@@ -0,0 +1,7 @@
+pub fn create_help() {
+    println!("IWDIL - I will do it later");
+    println!("---------------------------------");
+    println!("Usage: iwdil [command]");
+    println!("---------------------------------");
+    println!("Commands:");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use git2::Repository;
+
+mod help;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let args_len = args.len();
+
+    println!("Args: {}", args_len);
+
+    if args_len >= 2 {
+        println!("Too many arguments provided.");
+        println!("---------------------------------");
+        help::create_help();
+        return;
+    }
+
+    else if args_len > 1 {
+        println!("Argument: {}", args[1]);
+        return;        
+    }
+
+    else {
+        help::create_help();
+        return;
+    }
 }
